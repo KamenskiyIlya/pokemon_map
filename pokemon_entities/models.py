fd743fd3ec0e09bc53674e739c113477e4613c75
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-02-02 23:48:07 +0300

    Связал БД моделей Pokemon и PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,12 +1,14 @@
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
-	title = models.CharField(max_length=200)
-	photo = models.ImageField(upload_to='pokemon_imgs', null=True)
+	name = models.CharField(max_length=200)
+	photo = models.ImageField(upload_to='pokemon_imgs', null=True, blank=True)
 
 	def __str__(self):
-		return f'{self.title}'
+		return f'{self.name}'
 
 class PokemonEntity(models.Model):
+	pokemon = models.ForeignKey(Pokemon, on_delete= models.CASCADE)
 	lat = models.FloatField()
 	lon = models.FloatField()
+	
