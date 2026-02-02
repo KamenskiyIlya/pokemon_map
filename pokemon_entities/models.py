ef91d1c7c0abf31aa26f4e09cc61b925353a2d1e
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-02-02 23:58:41 +0300

    Добавил поля DateTime к модели PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,6 @@
 from django.db import models  # noqa F401
 
+
 class Pokemon(models.Model):
 	name = models.CharField(max_length=200)
 	photo = models.ImageField(upload_to='pokemon_imgs', null=True, blank=True)
@@ -11,4 +12,5 @@ class PokemonEntity(models.Model):
 	pokemon = models.ForeignKey(Pokemon, on_delete= models.CASCADE)
 	lat = models.FloatField()
 	lon = models.FloatField()
-	
+	appeared_at = models.DateTimeField()
+	disappeared_at = models.DateTimeField()
